29c44c8c416133551f3e9c80d2784104a8cb9dc2
LeticiaRutsatz <leticiarutsatz099@gmail.com>
2023-05-09 19:40:19 -0300

    teste

--- dist/app/shared/domain/enums/flow.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Flow = void 0;
-var Flow;
-(function (Flow) {
-    Flow["LIGTH"] = "LIGTH";
-    Flow["MODERATE"] = "MODERATE";
-    Flow["INTENSE"] = "INTENSE";
-})(Flow = exports.Flow || (exports.Flow = {}));
